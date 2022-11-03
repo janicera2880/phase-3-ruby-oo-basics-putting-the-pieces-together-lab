59be77d033563fb76e367a8216061667d85a6a69
Janice <janiceralecha@gmail.com>
2022-11-02 20:06:35 -0500

    commit tree 40bca21f0dae071db895b7c7af5a5949649f3215

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize title
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = "272"
+book.genre = "Mystery"
+
+puts book.title
+puts book.author
+puts book.page_count
+puts book.genre
+book.turn_page
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize brand
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+shoe1 = Shoe.new("Nike")
+shoe1.color = ("red")
+shoe1.size = ("9.5")
+shoe1.material = ("suede")
+shoe1.condition = ("tattered")
+
+puts shoe1.brand
+puts shoe1.color
+puts shoe1.size
+puts shoe1.material
+puts shoe1.condition
+shoe1.cobble
+puts shoe1.condition
